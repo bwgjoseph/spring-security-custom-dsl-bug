8c33aac979b33733d088c64f0ce81b3072e4930c
bwgjoseph <josephgan@live.com.sg>
2023-10-09 01:01:27 +0800

    register RequestHeaderAuthenticationFilter at DummyDsl

--- src/main/java/com/bwgjoseph/springsecuritycustomdslbug/DummyDsl.java
+++ src/main/java/com/bwgjoseph/springsecuritycustomdslbug/DummyDsl.java
@@ -9,6 +9,24 @@ public class DummyDsl extends AbstractHttpConfigurer<DummyDsl, HttpSecurity> {
     public void init(HttpSecurity http) throws Exception {
         http
             .formLogin(AbstractHttpConfigurer::disable);
+
+
+        // one possible alternative is to register RequestHeaderAuthenticationFilter at DummyDsl
+        // ApplicationContext context = http.getSharedObject(ApplicationContext.class);
+
+        // AuthenticationManager authenticationManager = context.getBean(AuthenticationManager.class);
+        // // actually i'm not sure how to grab generic type of bean from context correctly
+        // AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource = context.getBean(AuthenticationDetailsSource.class);
+
+        // RequestHeaderAuthenticationFilter requestHeaderAuthenticationFilter = new RequestHeaderAuthenticationFilter();
+        // requestHeaderAuthenticationFilter.setPrincipalRequestHeader("X-User");
+        // requestHeaderAuthenticationFilter.setExceptionIfHeaderMissing(true);
+        // requestHeaderAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        // requestHeaderAuthenticationFilter.setAuthenticationDetailsSource(authenticationDetailsSource);
+
+        // RequestAttributeSecurityContextRepository securityContextRepository =
+        //     new RequestAttributeSecurityContextRepository();
+        // requestHeaderAuthenticationFilter.setSecurityContextRepository(securityContextRepository);
     }
 
     public static DummyDsl dummyDsl() {
